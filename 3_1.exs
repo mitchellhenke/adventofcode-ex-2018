bf5d8bee74d6c10d02d41f39fcdaeed08e1f0371
Mitchell Henke <mitchh23@gmail.com>
2018-12-03 09:59:05 -0600

    Day 3

---
+++ 3_1.exs
@@ -0,0 +1,24 @@
+File.read!("data/3.txt")
+|> String.split("\n")
+|> Enum.reject(& &1 == "")
+|> Enum.reduce({MapSet.new, MapSet.new}, fn(row, {all_points, conflict_points}) ->
+  [_num, point_and_size] = String.split(row, "@ ")
+  [point, size] = String.split(point_and_size, ": ")
+
+  [x, y] = String.split(point, ",")
+           |> Enum.map(& String.to_integer(&1))
+
+  [width, height] = String.split(size, "x")
+           |> Enum.map(& String.to_integer(&1))
+
+  positions = Enum.reduce(y..y+height-1, MapSet.new, fn(y_position, acc_out) ->
+    Enum.reduce(x..x+width-1, acc_out, fn(x_position, acc) ->
+      MapSet.put(acc, [x_position, y_position])
+    end)
+  end)
+
+  {MapSet.union(all_points, positions), MapSet.union(conflict_points, MapSet.intersection(all_points, positions))}
+end)
+|> elem(1)
+|> MapSet.size
+|> IO.inspect()
