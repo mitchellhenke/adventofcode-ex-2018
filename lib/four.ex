b113899e4cd36f87350de8511abc560718562b2f
Mitchell Henke <mitchh23@gmail.com>
2018-12-04 15:49:16 -0600

    format

--- lib/four.ex
+++ lib/four.ex
@@ -5,14 +5,17 @@ defmodule Adventofcode2018.Four do
   @regex ~r/\[(?<year>\d+)-(?<month>\d+)-(?<day>\d+) (?<hour>\d+):(?<minute>\d+)\] (?<text>.*)/
   def first do
     minutes = process()
-    {number, minute_count} = Enum.max_by(minutes, fn({_guard_number, minute_count}) ->
-      Map.values(minute_count)
-      |> Enum.sum()
-    end)
 
-    {minute, _total} = Enum.max_by(minute_count, fn({_minute,  total}) ->
-      total
-    end)
+    {number, minute_count} =
+      Enum.max_by(minutes, fn {_guard_number, minute_count} ->
+        Map.values(minute_count)
+        |> Enum.sum()
+      end)
+
+    {minute, _total} =
+      Enum.max_by(minute_count, fn {_minute, total} ->
+        total
+      end)
 
     IO.inspect("Guard ##{number} slept the most overall. Mostly during minute #{minute}")
 
@@ -22,12 +25,13 @@ defmodule Adventofcode2018.Four do
   def second do
     minutes = process()
 
-    {guard_number, minute_count} = Enum.max_by(minutes, fn({_guard_id, minute_count}) ->
-      Map.values(minute_count)
-      |> Enum.max()
-    end)
+    {guard_number, minute_count} =
+      Enum.max_by(minutes, fn {_guard_id, minute_count} ->
+        Map.values(minute_count)
+        |> Enum.max()
+      end)
 
-    Enum.sort_by(minute_count, fn({_minute, count}) ->
+    Enum.sort_by(minute_count, fn {_minute, count} ->
       count * -1
     end)
     |> List.first()
@@ -39,32 +43,42 @@ defmodule Adventofcode2018.Four do
     {minutes, _final_state} =
       File.read!("data/4.txt")
       |> String.split("\n", trim: true)
-      |> Enum.map(fn(row) ->
+      |> Enum.map(fn row ->
         captures = Regex.named_captures(@regex, row)
-        {:ok, date} = NaiveDateTime.from_iso8601("#{captures["year"]}-#{captures["month"]}-#{captures["day"]} #{captures["hour"]}:#{captures["minute"]}:00")
+
+        {:ok, date} =
+          NaiveDateTime.from_iso8601(
+            "#{captures["year"]}-#{captures["month"]}-#{captures["day"]} #{captures["hour"]}:#{
+              captures["minute"]
+            }:00"
+          )
+
         {captures["text"], date}
       end)
-      |> Enum.sort_by(fn({_text, date}) ->
+      |> Enum.sort_by(fn {_text, date} ->
         {date.year, date.month, date.day, date.hour, date.minute}
       end)
-      |> Enum.reduce({%{}, %Four{}}, fn({text, date}, {map, state}) ->
+      |> Enum.reduce({%{}, %Four{}}, fn {text, date}, {map, state} ->
         cond do
           String.contains?(text, "begins shift") ->
             [_, _, number, _, _] = String.split(text, ["#", " "])
 
             {map, %Four{number: number, asleep: false, last_time: date}}
+
           text == "wakes up" ->
-            map = if(state.number) do
-              Enum.reduce(state.last_time.minute..date.minute-1, map, fn(minute, acc) ->
-                Map.update(acc, state.number, %{minute => 1}, fn(map_value) ->
-                  Map.update(map_value, minute, 1, &(&1 + 1))
+            map =
+              if(state.number) do
+                Enum.reduce(state.last_time.minute..(date.minute - 1), map, fn minute, acc ->
+                  Map.update(acc, state.number, %{minute => 1}, fn map_value ->
+                    Map.update(map_value, minute, 1, &(&1 + 1))
+                  end)
                 end)
-              end)
-            else
-              map
-            end
+              else
+                map
+              end
 
             {map, %Four{state | asleep: false, last_time: date}}
+
           text == "falls asleep" ->
             {map, %Four{state | asleep: true, last_time: date}}
         end
--- lib/two.ex
+++ lib/two.ex
@@ -2,21 +2,22 @@ defmodule Adventofcode2018.Two do
   def first do
     File.read!("data/2.txt")
     |> String.split("\n", trim: true)
-    |> Enum.reduce(%{two: 0, three: 0}, fn(box_id, acc) ->
+    |> Enum.reduce(%{two: 0, three: 0}, fn box_id, acc ->
       letters = String.graphemes(box_id)
       unique = MapSet.new(letters)
 
-      counted_letters = Enum.reduce_while(unique, MapSet.new, fn(letter, acc) ->
-        count = Enum.count(letters, & &1 == letter)
+      counted_letters =
+        Enum.reduce_while(unique, MapSet.new(), fn letter, acc ->
+          count = Enum.count(letters, &(&1 == letter))
 
-        set = MapSet.put(acc, count)
+          set = MapSet.put(acc, count)
 
-        if MapSet.subset?(set, MapSet.new([2, 3])) do
-          {:halt, set}
-        else
-          {:cont, set}
-        end
-      end)
+          if MapSet.subset?(set, MapSet.new([2, 3])) do
+            {:halt, set}
+          else
+            {:cont, set}
+          end
+        end)
 
       contains_two = MapSet.member?(counted_letters, 2)
       contains_three = MapSet.member?(counted_letters, 3)
@@ -24,10 +25,13 @@ defmodule Adventofcode2018.Two do
       case {contains_two, contains_three} do
         {true, true} ->
           %{acc | two: acc.two + 1, three: acc.three + 1}
+
         {true, false} ->
           %{acc | two: acc.two + 1}
+
         {false, true} ->
           %{acc | three: acc.three + 1}
+
         _ ->
           acc
       end
@@ -35,19 +39,22 @@ defmodule Adventofcode2018.Two do
   end
 
   def second do
-    ids = File.read!("data/2.txt")
-          |> String.split("\n", trim: true)
+    ids =
+      File.read!("data/2.txt")
+      |> String.split("\n", trim: true)
 
-    Enum.reduce_while(ids, ids, fn(box_id, acc) ->
-      differences = Enum.map(acc, fn(id) ->
-        distance = String.jaro_distance(box_id, id)
-        if distance > 0.974358974358974 && distance < 1.0 do
-          {box_id, id}
-        else
-          nil
-        end
-      end)
-      |> Enum.filter(& !is_nil(&1))
+    Enum.reduce_while(ids, ids, fn box_id, acc ->
+      differences =
+        Enum.map(acc, fn id ->
+          distance = String.jaro_distance(box_id, id)
+
+          if distance > 0.974358974358974 && distance < 1.0 do
+            {box_id, id}
+          else
+            nil
+          end
+        end)
+        |> Enum.filter(&(!is_nil(&1)))
 
       if Enum.count(differences) > 0 do
         {:halt, differences}
